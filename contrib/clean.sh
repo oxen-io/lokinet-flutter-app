a0f51fbfac372f15e9c21ef26e0edab1c301405f
Jeff <jeff@i2p.rocks>
2021-12-13 22:22:14 -0500

    fix previous commit

--- contrib/clean.sh
+++ contrib/clean.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 flutter clean
-cd "$(dirname $0)/../lokinet_lib/android" && gradle clean
-cd "$(dirname $0)/../" && git submodule foreach git clean -xdf
+git submodule foreach git clean -xdf
